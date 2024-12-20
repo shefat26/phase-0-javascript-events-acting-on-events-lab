377009f1259e3a2a018fdac75b2d6387006d7bd4
shefat26 <shefatmoral@gmail.com>
2024-12-19 20:50:51 -0500

    JS Event Acting on Event lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+ 
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger"); 
+  
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    
+    if (left < 360) { 
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
